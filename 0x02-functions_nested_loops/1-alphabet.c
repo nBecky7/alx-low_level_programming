5e999b2b14d49d233a49a687739b918d919fa0d6
BeckyN <nbecky14@gmail.com>
2022-09-14 01:14:55 -0700

    The Alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * print_alphabet -Entry point
+ * main - check the code
  * print the alphabet in lowercase fallowed by a new line
- * Return: (0).
+ * Return: Always 0.
  */
-void print_alphabet(void)
+int main(void)
 {
 	char i;
 
 	for (i = 'a'; i <= 'z'; i++)
 		_putchar(i);
 	_putchar('\n');
+	return (0);
 }
