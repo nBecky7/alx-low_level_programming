dd9e59c691207d1f973afc1c71d6d9c580b0dbdb
BeckyN <nbecky14@gmail.com>
2022-09-15 13:41:22 -0700

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_times_table - prints_times_table for numbers from 0-14
+ * print_times_table - prints times table for numbers from 0-14
  * @n: An input integer value
  * Return: Nothing
  */
