9cbdbdb6d8459de90d661126dabc437f75d8859c
BeckyN <nbecky14@gmail.com>
2022-09-15 05:58:17 -0700

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,53 +1,55 @@
 #include "main.h"
 
 /**
- * print_times_table - prints the times_table
- * @n: integer for which the times table will be printed
- *
- * Description: prints the times table
- *
- * Return: void
+ * print_times_table - prints_times_table for numbers from 0-14
+ * @n: An input integer value
+ * Return: Nothing
  */
-
 void print_times_table(int n)
 {
-	int row, column, product;
+	int i, j;
 
-	if (n >= 0 && n < 15)
+	if (n > 0 && n < 15)
 	{
-		for (row = 0; row <= n; row++)
+		for (i = 0; i <= n; i++)
 		{
-			for (column = 0; column <= n; column++)
-			{
-				product = row * column;
-				if (column == 0)
-					_putchar('0');
-				else if (product < 10)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(product % 10 + '0');
-				}
-				else if (product >= 10 && product < 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((product / 10) % 10 + '0');
-					_putchar(product % 10 + '0');
-				}
-				else if (product > 99 && product < 1000)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(product / 100 + '0');
-					_putchar((product / 10) % 10 + '0');
-					_putchar(product % 10 + '0');
-				}
-			}
-		_putchar('\n');
+			_putchar('0');
+			for (j = 1; j <= n; j++)
+				putformat(i * j);
+			_putchar('\n');
 		}
+	}
+}
+
+/**
+ * putformat - formatted characters to output
+ * @n: number to format
+ * Return: nothing
+ */
+void putformat(int n)
+{
+	if (n <= 9)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(n + '0');
+	}
+	else if (n > 9 && n <= 99)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+	else
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(n / 100 + '0');
+		_putchar(n / 10 % 10 + '0');
+		_putchar(n % 10 + '0');
 	}
 }
