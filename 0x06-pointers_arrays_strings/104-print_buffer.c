aa7ba14994c19cbcc5899e17d81b3b7f09774214
BeckyN <nbecky14@gmail.com>
2022-09-22 11:52:13 -0700

    Print some buffers

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -34,9 +34,9 @@ void print_buffer(char *b, int size)
 				break;
 
 			else if (*(b + index + byte) >= 31 &&
-					*(b + index + byte) <= 126)
+				*(b + index + byte) <= 126)
 				printf("%c", *(b + index + byte));
-
+			
 			else
 				printf(".");
 		}
@@ -49,7 +49,4 @@ void print_buffer(char *b, int size)
 
 	if (size <= 0)
 		printf("\n");
-}
-	
-
-		
+}		
