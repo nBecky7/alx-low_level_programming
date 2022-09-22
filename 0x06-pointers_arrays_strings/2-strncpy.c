614eff8af7675a506f4afda2eb8de76ca70d0c39
BeckyN <nbecky14@gmail.com>
2022-09-22 07:48:06 -0700

    Copying integers

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,22 +1,32 @@
 #include "main.h"
+
 /**
- * *_strncpy - find the length of a string
- * @dest: pointer to the string
- * @src: pointer
- * @n: int
- * Return: destination
+ * _strncpy - a function that copys the strings.
+ * @dest: an input string
+ * @src: an input string
+ * @n: an input integer
+ * Return: A pointer to the resulting string
  */
-
-
 char *_strncpy(char *dest, char *src, int n)
-[
-int i;
+{
+	int srclen = 0, i = 0;
+	char *temp = dest, *start = src;
 
-for (i = 0; i < n && src[i] != '\0'; i++)
-        dest[i] = src[i];
+	while (*src)
+	{
+		srclen++;
+		src++;
+	}
 
-for ( ; i < n; i++)
-        dest[i] = '\0';
+	srclen++;
 
-return (dest);
+	if (n > srclen)
+		n = srclen;
+
+	src = start;
+
+	for (; i < n; i++)
+		*dest++ = *src++;
+
+	return (temp);
 }
