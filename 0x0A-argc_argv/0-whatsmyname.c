cad89e881e0c8091212dcff2cabcba9be31995d7
BeckyN <nbecky14@gmail.com>
2022-10-05 02:13:54 -0700

    Printing of a name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,18 +1,15 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
  *  main - program that prints its name, followed by a new line
- *
  *  @argc: argument count
  *  @argv: argument vector
  *
  *  Return: 0
  */
-
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
-	(void)argc;
+
 	return (0);
 }
