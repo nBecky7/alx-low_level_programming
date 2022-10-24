0d0f542e950826fdf27c94ebba7918aee2dd4543
BeckyN <nbecky14@gmail.com>
2022-10-24 13:17:29 -0700

    Linked lists task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,18 +8,16 @@
  */
 size_t print_list(const list_t *h)
 {
-	unsigned int nodes = 0;
+	int counter = 0;
 
 	while (h)
 	{
-		printf("[%u] ", h->len);
 		if (h->str == NULL)
-			printf("(nil)\n");
+			printf("[0] (nil)\n");
 		else
-			printf("%s\n", h->str);
-
+			printf("[%i] %s\n", h->len, h->str);
 		h = h->next;
-		nodes++;
+		counter++;
 	}
-	return (nodes);
+	return (counter);
 }
